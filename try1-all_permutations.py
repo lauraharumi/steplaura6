33f9a3ccca1c9bfe87494265b7c8844c697e1e98
Laura Harumi <laura.vazquez.fukazawa@gmail.com>
2016-07-04 00:57:09 +0900

    cheat -all permutations

--- try1-all_permutations.py
+++ try1-all_permutations.py
@@ -7,24 +7,24 @@ from common import print_solution, read_input
 import time
 start_time = time.time()
 
-def calc_distance(city1, city2):
+# This code tries all permutaions - obviously not a good way (wanted to try it anywayws)
+# Only works for input_0.csv and input_1.csv 
+
+def distance(city1, city2):
     city1, city2 = xy_list[city1], xy_list[city2] #city index to coordinate
     return math.sqrt((city1[0] - city2[0]) ** 2 + (city1[1] - city2[1]) ** 2)
 
 def try_everything(xy_list):
     shortest_dist= 100000000000000
-    first_last = 0
+    start_city = 0
     perms = itertools.permutations(range(1,len(xy_list)), len(xy_list)-1)
     for perm in perms:
-        first_step =  calc_distance(first_last, perm[0])
-        last_step = calc_distance(perm[len(perm)-1], first_last)
+        total_dist = distance(start_city, perm[0]) + distance(perm[len(perm)-1], start_city)
         i,j = 0, 1 
-        in_between = 0 
         while i < (len(perm)-1) and j < len(perm):
-            in_between += calc_distance(perm[i], perm[j])
+            total_dist += distance(perm[i], perm[j])
             i+= 1
             j+=1 
-        total_dist = first_step+in_between+last_step
         if total_dist < shortest_dist:
             shortest_dist, best_route = total_dist, perm
     return best_route, shortest_dist
